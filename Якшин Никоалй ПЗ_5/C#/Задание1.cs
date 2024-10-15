98c880354f2484b8fc2fdeefd91679c6839d445a
YakshinNikolai <nikolai1115@bk.ru>
2024-10-15 10:10:37 +0300

    Add files via upload

---
+++ Якшин Никоалй ПЗ_5/C#/Задание1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПЗ5
+{
+    internal class Задание1
+    {
+        static void Main(string[] args)
+        {
+            int i = Convert.ToInt32(Console.ReadLine());
+            int o= Convert.ToInt32(Console.ReadLine());
+            if (i >= o)
+            {
+                Console.WriteLine(i);
+            }
+            else
+            {
+                Console.WriteLine(o);
+            }
+        }
+    }
+}
---
+++ Якшин Никоалй ПЗ_5/C#/Задание2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПЗ5
+{
+    internal class Задание2
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("введите поочередно длины двух катетов");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            double z = Math.Sqrt(x * x + y * y);
+            Console.WriteLine(z);
+        }
+    }
+}
